bc1b6d4bd311e55dc5afa8b8438ee108f57d8e79
Drewry Pope <drewrypope@gmail.com>
2023-10-03 23:40:50 -0500

    Update overrides.nix

--- overrides.nix
+++ overrides.nix
@@ -200,7 +200,8 @@ let
     cmp-tmux
     kanagawa-nvim
     litee-nvim
-    neodev-nvim
+    # lua-dev-nvim
+    #neodev-nvim
     mini-nvim
     nvim-ts-autotag
     # onedark-nvim is an alias to onedark-pro-nvim.
