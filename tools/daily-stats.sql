c63f8aea0431ddcde579a8792520e9f08711d918
NACAMURA Mitsuhiro <m15@m15a.dev>
2024-04-09 23:32:50 +0900

    tools: styling

--- tools/daily-stats.sql
+++ tools/daily-stats.sql
@@ -30,14 +30,13 @@ UNION
       ON COLUMNS(* EXCLUDE (source, time, datetime))
     INTO NAME site VALUE plugins);
 
-
 CREATE TABLE daily AS
 SELECT date_trunc('day', datetime) AS "date"
      , source
      , site
      , max(plugins) AS plugins
   FROM stats
- GROUP BY source, site, date
+ GROUP BY date, source, site
  ORDER BY date, source, site;
 
 COPY daily TO 'data/stats/view/daily.csv'
