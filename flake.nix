0d332d962b264dd641c61491e1eabd3d56e92192
NACAMURA Mitsuhiro <m15@m15a.dev>
2024-04-10 09:15:56 +0900

    fix(nix): update old flake description

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Nix flake of miscellaneous Vim/Neovim plugins";
+  description = "Nix flake of Awesome Neovim plugins";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
