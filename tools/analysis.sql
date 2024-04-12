060eb1044040d0736aa17aa9d3ec5f912e8c07a1
NACAMURA Mitsuhiro <m15@m15a.dev>
2024-04-13 07:55:27 +0900

    tools: add SQL to analyze generated data

---
+++ tools/analysis.sql
@@ -0,0 +1,25 @@
+-- Run `duckdb -init tools/analysls.sql` and then you can analyze
+-- `data/plugins-info/awesome-neovim.json`.
+
+DROP TABLE IF EXISTS plugins;
+CREATE TABLE plugins AS
+SELECT site
+     , owner
+     , repo
+     , description
+     , homepage
+     , license
+     , date
+     , rev
+     , sha256
+  FROM read_json( 'data/plugins-info/awesome-neovim.json'
+                , columns = { site: 'VARCHAR'
+                            , owner: 'VARCHAR'
+                            , repo: 'VARCHAR'
+                            , description: 'VARCHAR'
+                            , homepage: 'VARCHAR'
+                            , license: 'VARCHAR'
+                            , date: 'DATE'
+                            , rev: 'VARCHAR'
+                            , sha256: 'VARCHAR'
+                            });
