3f99e952e17a076bfbbedc92090982bbaaf7770a
NACAMURA Mitsuhiro <m15@m15a.dev>
2024-09-08 12:42:46 +0900

    refactor: simplify SQL

--- tools/daily-stats.sql
+++ tools/daily-stats.sql
@@ -1,29 +1,24 @@
 #!/usr/bin/env -S duckdb -batch -no-stdin -init
 
-CREATE OR REPLACE TABLE stats
-( repository VARCHAR
-, datetime TIMESTAMP
-, site VARCHAR 
-, plugins INT
-);
-
-CREATE OR REPLACE MACRO unpivot_stats(src_name, src_path) AS TABLE
-UNPIVOT (SELECT src_name AS repository
+CREATE OR REPLACE MACRO unpivot_stats(path) AS TABLE
+UNPIVOT (SELECT regexp_extract(path, '([^/]+)/[^/]+\.json$', 1) AS repository
               , make_timestamp(time * 1e6 :: BIGINT) AS datetime
               , * EXCLUDE time
-           FROM read_json_auto(src_path, union_by_name = true))
+           FROM read_json_auto(path, union_by_name = true))
      ON COLUMNS(* EXCLUDE (repository, datetime))
    INTO NAME site VALUE plugins;
 
-INSERT INTO stats
+
+CREATE OR REPLACE TABLE stats AS
 SELECT *
-  FROM unpivot_stats('awesome-neovim', 'data/stats/awesome-neovim/*.json')
+  FROM unpivot_stats('data/stats/awesome-neovim/*.json')
  UNION
 SELECT *
-  FROM unpivot_stats('nixpkgs', 'data/stats/nixpkgs/*.json')
+  FROM unpivot_stats('data/stats/nixpkgs/*.json')
  UNION
 SELECT *
-  FROM unpivot_stats('extra', 'data/stats/extra/*.json');
+  FROM unpivot_stats('data/stats/extra/*.json');
+
 
 CREATE OR REPLACE TABLE daily AS
 SELECT date_trunc('day', datetime) AS "date"
