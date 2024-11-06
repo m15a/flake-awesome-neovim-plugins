fc63f517b578aa9e5bf515fb9a692beabca8c0c8
NACAMURA Mitsuhiro <m15@m15a.dev>
2024-11-06 20:35:50 +0900

    tools: better plot

--- tools/daily-plot.R
+++ tools/daily-plot.R
@@ -9,75 +9,31 @@ library(tidyr)
 
 daily_stats <- read_csv(
     file.path("data", "stats", "view", "daily.csv"),
-    col_types = c(
-        date = "D",
-        repository = "c",
-        site = "c",
-        plugins = "i"
-    )
+    col_types = c(date = "D", repository = "c", site = "c", plugins = "i")
 ) |>
     filter(site == "total") |>
     select(date, repository, plugins)
 
-monthly_stats <- daily_stats |>
-    mutate(month = floor_date(date, "month")) |>
-    group_by(month, repository) |>
-    filter(date == max(date)) |>
-    ungroup() |>
-    select(
-        date = date,
-        repository = repository,
-        plugins = plugins
-    )
-
-df <- monthly_stats |>
+df <- daily_stats |>
     mutate(
         repository = repository |>
-            ordered(
-                levels = c(
-                    "nixpkgs",
-                    "awesome-neovim",
-                    "extra"
-                )
-            ) |>
+            ordered(levels = c("nixpkgs", "awesome-neovim", "extra")) |>
             recode(
-                "awesome-neovim" = "Awesome Neovim",
-                "nixpkgs" = "Nixpkgs",
+                "awesome-neovim" = "Awesome Neovim plugins",
+                "nixpkgs" = "Nixpkgs Vim/Neovim plugins",
                 "extra" = "In this flake but not in Nixpkgs"
             )
     ) |>
-    select(
-        Date = date,
-        Repository = repository,
-        Plugins = plugins,
-    )
+    select(Date = date, Repository = repository, Plugins = plugins)
 
-g <- df |>
-    ggplot(
-        aes(
-            Date,
-            Plugins,
-            group = Repository,
-            color = Repository,
-            label = Plugins
-        )
-    ) +
+g <- ggplot(df, aes(Date, Plugins, color = Repository)) +
     geom_line() +
-    geom_point() +
-    geom_text(
-        vjust = ifelse(df$Date == max(df$Date), -.6, 1.6),
-        size = 3,
-        show.legend = FALSE,
-    ) +
-    scale_color_manual(
-        values = c("#5277C3", "#57A143", "#FF5F00")
-    ) +
-    scale_x_date(
-        date_breaks = "1 month",
-        date_minor_breaks = "1 week",
-    ) +
-    ylim(c(0, 2e3)) +
-    ggtitle("Number of Vim/Neovim plugins contained in each repository")
+    # geom_point(size = .01) +
+    scale_color_manual(values = c("#5277C3", "#57A143", "#FF5F00")) +
+    scale_x_date(date_breaks = "month") +
+    scale_y_continuous(labels = scales::label_number(suffix = "K", scale = 1e-3)) +
+    ggtitle("Number of Vim/Neovim plugins contained in each repository") +
+    theme(axis.text.x = element_text(angle = 30, vjust = 1, hjust = 0.98))
 
 ggsave(
     file.path("data", "stats", "plot", "daily.png"),
