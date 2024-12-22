86b79da5fd6dabe4b3e0ba484d36ff3e498a2377
NACAMURA Mitsuhiro <m15@m15a.dev>
2024-12-22 15:34:57 +0900

    ci: use treefmt-nix

--- nix/ci.nix
+++ nix/ci.nix
@@ -3,41 +3,9 @@ final: prev:
 with final;
 
 rec {
-  checks = {
-    format =
-      runCommand "check-format"
-        {
-          src = ../.;
-          nativeBuildInputs = [ nixfmt-rfc-style ];
-        }
-        ''
-          set -e
-          nixfmt --check --width=80 $src/*.nix $src/nix/
-          touch $out
-        '';
-
-    lint =
-      runCommand "check-lint"
-        {
-          src = ../.;
-          nativeBuildInputs = [
-            statix
-            deadnix
-          ];
-        }
-        ''
-          set -e
-          statix check $src/
-          deadnix --fail --no-lambda-arg --no-lambda-pattern-names $src/
-          touch $out
-        '';
-  };
-
   devShells = rec {
     default = mkShell {
       inputsFrom = [
-        checks.format
-        checks.lint
         ci-update
         ci-datasci
       ];
