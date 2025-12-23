38522e20d4bf02ef30a96e52b7bd93c1ce8ca8a5
NACAMURA Mitsuhiro <m15@m15a.dev>
2025-12-23 21:18:53 +0900

    chore: add formatter

--- nix/default.nix
+++ nix/default.nix
@@ -30,4 +30,22 @@ rec {
   packages = forSystems (pkgs: filterVimPlugins pkgs.awesomeNeovimPlugins);
 
   checks = mapAttrs (_: ps: mapAttrNames (n: "check-${n}") ps) packages;
+
+  formatter = forSystems (
+    pkgs:
+    pkgs.writeShellApplication {
+      name = "flake-awesome-neovim-plugins-formatter";
+      runtimeInputs = [ pkgs.nixfmt ];
+      text = ''
+        mapfile -t files < <(git ls-files --exclude-standard)
+        for file in "''${files[@]}"; do
+            case "''${file##*.}" in
+                nix)
+                    nixfmt -w88 "$file" &
+                    ;;
+            esac
+        done
+      '';
+    }
+  );
 }
